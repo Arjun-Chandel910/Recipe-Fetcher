bb74f90d77de11ee0836d4f95c989d70cf1c3cb3
Arjun Chandel <arjunchandel910@gmail.com>
2025-01-18 14:13:04 +0530

    enhanced styling

--- index.js
+++ index.js
@@ -1,24 +1,74 @@
 let form = document.querySelector("form");
 let srcBtn = document.querySelector("srcBtn");
 let inp = document.querySelector("input");
+let body = document.querySelector("body");
+
+// divs
 let resDiv = document.querySelector(".res");
 let beforeRes = document.querySelector(".beforeRes");
+let reciCard = document.querySelector(".reciCard");
 
+// Main CODE
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
+  resDiv.innerText = "";
 
   let recipies = await getRecipe(inp.value);
   for (r of recipies.data.meals) {
-    console.log(r.strMeal);
+    console.log(r);
     try {
       beforeRes.style.display = "none";
       // card
       let card = document.createElement("div");
       card.classList.add("cards");
 
-      // btn
+      // Details Button
       let details = document.createElement("button");
       details.innerText = "details";
+
+      // recipies
+      details.addEventListener(
+        "click",
+        ((recipeData) => {
+          return () => {
+            resDiv.style.opacity = 0.3;
+            reciCard.style.zIndex = 2;
+            reciCard.innerHTML = ""; // Clear previous details
+
+            let ul = document.createElement("ul");
+            for (let i = 1; i <= 11; i++) {
+              if (
+                recipeData[`strIngredient${i}`] &&
+                recipeData[`strMeasure${i}`]
+              ) {
+                let l = document.createElement("li");
+                l.innerText = `${recipeData[`strIngredient${i}`]} : ${
+                  recipeData[`strMeasure${i}`]
+                }`;
+                ul.appendChild(l);
+              }
+            }
+            let i = document.createElement("i");
+            i.classList.add("fa-sharp");
+            i.classList.add("fa-regular");
+            i.classList.add("fa-circle-xmark");
+            i.classList.add("cross");
+
+            reciCard.appendChild(i);
+            i;
+
+            reciCard.appendChild(ul);
+            reciCard.classList.remove("dis");
+            i.addEventListener("click", () => {
+              reciCard.classList.add("dis");
+              resDiv.style.opacity = 1;
+              reciCard.style.zIndex = 1;
+            });
+            reciCard.classList.add("recipeCard");
+          };
+        })(r)
+      );
+      // recipe list ends
 
       // img
       let img = document.createElement("img");
